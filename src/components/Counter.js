a2fbf3779790729f8378009130c6181a22aeeb48
김지혜 <k_jihye92@naver.com>
2024-02-23 13:31:28 +0900

    manage state by using useReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 export default function Counter() {
-  const [number, setNumber] = useState(0);
+  // const [number, setNumber] = useState(0);
+
+  function reducer(state,action) {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state +1;
+      case 'DECREMENT':
+        return state -1;
+
+      default:
+        throw new Error('unhandled action');
+    }
+  }
+  const [number, dispatch] = useReducer(reducer,0); // 함수와 초기값
+
   const onIncrease = () =>{
-    setNumber(prev => prev + 1)
+    // setNumber(prev => prev + 1)
+    dispatch({
+      type: 'INCREMENT'
+    })
   }
   const onDecrease = () =>{
-    setNumber(prev => prev -1 )
+    // setNumber(prev => prev -1 )
+    dispatch({
+      type: 'DECREMENT'
+    })
   }
   return (
     <>
